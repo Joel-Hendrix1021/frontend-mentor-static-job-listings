84a53b4acbe9f7a062f9c62ba53bc95fbdc1a0b2
Joel Pimentel <joelpimentel1995@gmail.com>
2021-09-03 15:18:16 -0500

    refactorizar code filter

--- src/App.js
+++ src/App.js
@@ -9,70 +9,42 @@ import ContainerFilter from "./components/ContainerFilter/ContainerFilter";
 function App() {
   const [data, setData] = useState(Dbdata)
   const [itemFilter, setItemFilter] = useState([])
- // const [statusFilter, setStatusFilter] =useState(false)
-
 
   useEffect(() => {
-      filterLenguajes()
+      setData(Dbdata)
   }, [itemFilter])
 
-  const handleSelectFilter=(query)=>{
-    
+  const filterSkills=({languages,level,role,tools})=>{
+
+       if(itemFilter.length === 0){
+         return true
+       }
+       const skillsArra = [...languages, level]
+       if(role) skillsArra.push(role)
+       if(tools) skillsArra.push(...tools)
+      return skillsArra.some(f=> itemFilter.includes(f))
+  }
+
+  const handleSelectFilter=(query)=>{  
    if(itemFilter.some(item=> item === query)) return itemFilter
    setItemFilter([
       ...itemFilter,query
     ])
-    setData(Dbdata)
   }
-
-//filtrar 
-  const filterLenguajes=()=>{
-       //console.log(itemFilter)
-       let  auxArray = []
-       if(itemFilter.length > 0){
-         for (let i = 0; i < data.length; i++) {
-           //filter lenguages
-           //itemFilter.map(item=> (data[i].languages.includes(item))? auxArray.push(data[i]) : null)   
-           itemFilter.forEach(item=> {
-             //lenguages
-             if(data[i].languages.includes(item)){
-               if(!auxArray.includes(data[i])){
-                auxArray.push(data[i])
-               }
-               //tools
-             }else if(data[i].tools.includes(item)){
-              if(!auxArray.includes(data[i])){
-                auxArray.push(data[i])
-               }
-             }else if(data[i].role.includes(item)){
-              if(!auxArray.includes(data[i])){
-                auxArray.push(data[i])
-               }
-             }else if(data[i].level.includes(item)){
-              if(!auxArray.includes(data[i])){
-                auxArray.push(data[i])
-               }
-             }
-           })
-           
-         }
-         console.log(auxArray)
-         setData(auxArray)
-        //setData(data.filter(el=> el.languages.includes())) 
-    }      
-} 
 
 const removeFilter=(n)=>{
   setItemFilter(itemFilter.filter(item=> item !==n))
-  //console.log(n)
-  setData(Dbdata)
+ 
 }
+//filtra basado en filterskills si es true devuelve el array
+const listSkills = data.filter(fil=> filterSkills(fil))
+
  
   return (
     <div className="App">
       <Header/>
         <ContainerFilter removeFilter={removeFilter} itemFilter={itemFilter}/>
-       <Table handleSelectFilter={handleSelectFilter} data={data}/>
+       <Table handleSelectFilter={handleSelectFilter} data={listSkills}/>
     </div>
   );
 }
